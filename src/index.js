0b49f004a17f9137b84206c473fab2ddc1e80034
monalice <alice.amorim@engenharia.ufjf.br>
2021-07-30 17:24:03 -0300

    heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app');
 
-app.listen(process.env.PORT, () => {
+app.listen(process.env.PORT || 8000, () => {
     console.log(`Server is running at port ${process.env.PORT}`)
 });
