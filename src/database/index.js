2b17973f50a9b90617ec3656215b1461961cd878
monalice <alice.amorim@engenharia.ufjf.br>
2021-07-30 18:01:11 -0300

    heroku

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 require('dotenv').config();
 const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize(process.env.URI, {
+const sequelize = new Sequelize(process.env.DATABASE_URL, {
     dialect: 'postgres',
     logging: false
 });
