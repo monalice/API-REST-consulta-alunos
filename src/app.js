160167f01298bedd72ab6ed535eefaf0f868729a
monalice <alice.amorim@engenharia.ufjf.br>
2021-07-30 18:50:05 -0300

    heroku

--- src/app.js
+++ src/app.js
@@ -3,12 +3,6 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors());
-var corsOptions = {
-    origin: "*",
-    optionsSuccessStatus: 200,
-    methods : '*'
-}
-app.use(cors(corsOptions));
 app.use(express.json());
 
 const getProducts = require('./routers/studentRouter');
