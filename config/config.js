5741af6bfd5fcf60b793eec130c304738c6dd276
monalice <alice.amorim@engenharia.ufjf.br>
2021-07-30 17:25:30 -0300

    heroku

--- config/config.js
+++ config/config.js
@@ -5,9 +5,11 @@ dotenv.config();
 module.exports = {
   development: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
   test: {
     url: process.env.DATABASE_URL,
+    dialect: 'postgres',
   },
   production: {
     url: process.env.DATABASE_URL,
